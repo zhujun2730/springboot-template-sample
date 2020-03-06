ef43b70a96d828fe35b023cc5dc61440a993c089
zhuj <zhuj@fapiao.com>
2020-03-06 17:03:30 +0800

    remove pages

--- src/main/java/com/egreat/movie/api/ApiApplication.java
+++ src/main/java/com/egreat/movie/api/ApiApplication.java
@@ -1,12 +1,10 @@
 package com.egreat.movie.api;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@MapperScan(value = {"com.egreat.movie.api.dao"})
 public class ApiApplication {
 
 	public static void main(String[] args) {
